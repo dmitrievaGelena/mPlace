5abc0cab4e149128060a62ba6684ef7abaa8dd80
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-03-19 13:55:20 +0300

    add header and section search

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function() {
 
 });
+
+const btnView = document.querySelector('.btn-view'),
+      btnProduct = document.querySelector('.btn-product');
+
+
+function changeBtn() {
+    btnView.addEventListener('click', (ev) => {
+        ev.target.classList.add('active_btn');
+        btnProduct.classList.remove('active_btn');
+    });
+    btnProduct.addEventListener('click', (even) => {
+        even.target.classList.add('active_btn');
+        btnView.classList.remove('active_btn');
+    });
+}
+changeBtn();
+
+
