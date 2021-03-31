12b3b659568f9945f1e5a383d24f295a00a6c00f
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-03-31 17:56:57 +0300

    add section follow, benefits, features

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,48 @@
-$(function() {
-    $("#rateYo").rateYo({
+$(function () {
+    $(".rate-star").rateYo({
         rating: 4.2,
         starWidth: "15px",
         readOnly: true
-      });
+    });
+    $('.slider__info').slick({
+        prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>'
+    });
+    $('.follow__info-item').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button class="slick-arrow slick-prev follow__slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button class="slick-arrow slick-next follow__slick-next"><span class="lnr lnr-chevron-right"></span></button>'
+    });
+
+    var mixer = mixitup('.products__info-box');
+
 });
 
+
 const btnView = document.querySelector('.btn-view'),
-      btnProduct = document.querySelector('.btn-product');
+    btnProduct = document.querySelector('.btn-product');
 
 
 function changeBtn() {
     btnView.addEventListener('click', (ev) => {
+        ev.preventDefault();
         ev.target.classList.add('active_btn');
         btnProduct.classList.remove('active_btn');
     });
     btnProduct.addEventListener('click', (even) => {
+        even.preventDefault();
         even.target.classList.add('active_btn');
         btnView.classList.remove('active_btn');
     });
 }
 changeBtn();
 
+const filterBtn = document.querySelector('.icon-more-vertical'),
+    filterList = document.querySelector('.filter__title-list'),
+    filterParent = document.querySelector('.products__inner');
 
+filterBtn.addEventListener('click', () => {
+    if (filterList.classList.toggle('hide'));
+});
